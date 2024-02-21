a030ac175ed02cd329dbd084e363975cef2210b9
BastienCarbonnier <bastien.carbonnier@gmail.com>
2024-02-21 02:08:55 -0500

    main: Update Test

--- src/test/java/com/carbonnb/urlshortener/integrations/PostgresqlContainerBaseTest.java
+++ src/test/java/com/carbonnb/urlshortener/integrations/PostgresqlContainerBaseTest.java
@@ -1,33 +1,37 @@
 package com.carbonnb.urlshortener.integrations;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-abstract class AbstractContainerBaseTest {
-
-    static final PostgreSQLContainer<?> POSTGRES_SQL_CONTAINER;
-
-    static {
-        POSTGRES_SQL_CONTAINER = new PostgreSQLContainer<>(
-                "postgres:15-alpine"
-        );
-        POSTGRES_SQL_CONTAINER.start();
-    }
-}
-
-public class PostgresqlContainerBaseTest extends AbstractContainerBaseTest {
+public class PostgresqlContainerBaseTest {
     @LocalServerPort
     private Integer port;
 
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:15-alpine"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES_SQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES_SQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", POSTGRES_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
     }
 
     @BeforeEach
