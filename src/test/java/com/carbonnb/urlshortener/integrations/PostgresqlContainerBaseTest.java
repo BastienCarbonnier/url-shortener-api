421e7a2fdac2c62d881bfa18bc4c5ee102345160
BastienCarbonnier <bastien.carbonnier@gmail.com>
2024-02-21 02:13:08 -0500

    main: Update Test

--- src/test/java/com/carbonnb/urlshortener/integrations/PostgresqlContainerBaseTest.java
+++ src/test/java/com/carbonnb/urlshortener/integrations/PostgresqlContainerBaseTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+@DirtiesContext
 public class PostgresqlContainerBaseTest {
     @LocalServerPort
     private Integer port;
